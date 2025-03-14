3ddfa5ced5ecae16bfcfb60d39a98f27f495ce9b
Alexandra1123 <al.h8500@gmail.com>
2025-03-14 22:50:59 +0400

    Enviar notificaciones de disputa

--- src/middlewares/notification.ts
+++ src/middlewares/notification.ts
@@ -3,6 +3,15 @@ import axios from "axios";
 import { NotificationI } from "../interfaces/notification.interface";
 import { NotificationAction } from "../utils/Types";
 
+function sendNotification(notification: NotificationI | undefined) {
+  if (notification?.targetId && notification?.receiverId)
+    axios
+      .post(`${process.env.API_USER}notification/send`, notification)
+      .catch((error) => {
+        console.error("Error al enviar notificación:", error);
+      });
+}
+
 export const saveNotificationMiddleware = (
   req: Request,
   res: Response,
@@ -20,26 +29,45 @@ export const saveNotificationMiddleware = (
         req.body.notification
       ) {
         notificationSaved = true;
-        const notification: NotificationI = req.body.notification;
-        // Descargar orden de oferta seleccionada
-        if (
-          notification.action == NotificationAction.DOWNLOAD_PURCHASE_ORDER &&
-          !notification.targetId
-        ) {
-          notification.targetId = body.res?.purchaseOrderUID;
+        let notification: NotificationI | undefined = req.body.notification;
+        const extraNotifications: NotificationI[] | undefined =
+          req.body.extraNotifications;
+        if (notification) {
+          // Descargar orden de oferta seleccionada
+          if (
+            notification.action == NotificationAction.DOWNLOAD_PURCHASE_ORDER &&
+            !notification.targetId
+          ) {
+            notification.targetId = body.res?.purchaseOrderUID;
+          }
+          // Cancelar 'mi' oferta seleccionada
+          else if (
+            notification.action == NotificationAction.VIEW_REQUIREMENT &&
+            !notification.receiverId
+          ) {
+            notification.receiverId = body.res?.requirementSubUserUid; // creador del requerimiento
+          }
+          // Se produjo una disputa al culminar
+          else if (
+            (notification.action == NotificationAction.VIEW_OFFER ||
+              notification.action == NotificationAction.VIEW_REQUIREMENT) &&
+            body.res?.dispute
+          ) {
+            notification = undefined;
+            if (Array.isArray(extraNotifications)) {
+              // Enviar notificaciones de disputa
+              extraNotifications.forEach((notification) => {
+                sendNotification(notification);
+              });
+            }
+          }
+
+          // Enviar notificación si existe
+          sendNotification(notification);
         }
-        // Cancelar 'mi' oferta seleccionada
-        else if (
-          notification.action == NotificationAction.VIEW_REQUIREMENT &&
-          !notification.receiverId
-        ) {
-          notification.receiverId = body.res?.requirementSubUserUid; // creador del requerimiento
-        }
-        if (notification.targetId && notification.receiverId)
-          axios.post(`${process.env.API_USER}notification/send`, notification);
       }
     } catch (e) {
-      console.log("Error al enviar notificación", e);
+      console.log("Error en saveNotificationMiddleware", e);
     }
 
     return originalSend(body);
