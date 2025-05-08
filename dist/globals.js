88b7ad848981451abe34d17385c26746a35c2133
EdwinLlave <maxedwin8@gmail.com>
2025-05-08 11:25:13 -0500

    Se a corregido el dist

--- dist/globals.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.subUserRoomName = exports.timeNotificationNewRequirements = void 0;
-exports.timeNotificationNewRequirements = 45; // minutos
-exports.subUserRoomName = "subUser";
-//# sourceMappingURL=globals.js.map
--- dist/initConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.igv = void 0;
-exports.igv = 18;
-//# sourceMappingURL=initConfig.js.map
