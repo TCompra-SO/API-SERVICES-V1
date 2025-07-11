a89371fea9d1b59239302777765988a2e17e17c1
Alexandra1123 <al.h8500@gmail.com>
2025-07-12 00:46:37 +0400

    CSRF token añadido / verificación de solicitudes de origen confiable

--- src/globals.ts
+++ src/globals.ts
@@ -1,4 +1,6 @@
 export const timeNotificationNewRequirements = 45; // minutos
 export const subUserRoomName = "subUser";
-export const accessTokenName = "accessToken";
-export const refreshTokenName = "refreshToken";
+export const accessTokenName = "AC-TC-T";
+export const refreshTokenName = "RF-TC-T";
+export const csrfTokenName = "TC-CS-T";
+export const secretInternalName = "x-internal-auth";
--- src/middlewares/session.ts
+++ src/middlewares/session.ts
@@ -1,24 +1,51 @@
 import { NextFunction, Request, Response } from "express";
 import { verifyToken } from "../utils/jwt.handle";
 import { RequestExt } from "./../interfaces/req-ext";
-import { accessTokenName } from "../globals";
+import { accessTokenName, csrfTokenName, secretInternalName } from "../globals";
+
+// Verificar si solicitud proviene de otro de los servidores de TCompra
+function checkTrust(req: RequestExt): boolean {
+  if (process.env.INTERNAL_SECRET) {
+    const secret = req.get(secretInternalName);
+    if (secret === process.env.INTERNAL_SECRET) {
+      return true;
+    }
+  }
+  return false;
+}
 
 const checkJwt = async (req: RequestExt, res: Response, next: NextFunction) => {
   try {
-    // const jwtByUser = req.headers.authorization || null;
-    const jwtByUser = req.cookies || null;
-    if (!jwtByUser) {
-      return res.status(401).send({
-        success: false,
-        code: 401,
-        error: {
-          msg: "NO_TIENES_UN_JWT_VALIDO",
-        },
-      }); // Sin token en los headers
+    const isTrusted = checkTrust(req);
+
+    if (!isTrusted) {
+      // Double Submit Cookie
+      const csrfCookieToken = req.cookies[csrfTokenName];
+      const csrfHeaderToken = req.get(csrfTokenName);
+      if (!csrfCookieToken || !csrfHeaderToken) {
+        return res.status(401).send({
+          success: false,
+          code: 401,
+          error: {
+            msg: "NO_TIENES_UN_CSRF_TOKEN",
+          },
+        });
+      }
+
+      if (csrfCookieToken !== csrfHeaderToken) {
+        return res.status(401).send({
+          success: false,
+          code: 401,
+          error: {
+            msg: "CSRF_TOKEN_INVALIDO",
+          },
+        });
+      }
     }
 
-    // const jwt = jwtByUser.split(" ")[1]; // Usar el índice 1 para obtener el token después de "Bearer"
-    const jwt = req.cookies[accessTokenName];
+    const jwt = isTrusted
+      ? req.headers.authorization?.split(" ")[1] // Obtener el token después de "Bearer"
+      : req.cookies?.[accessTokenName];
     if (!jwt) {
       return res.status(401).send({
         success: false,
@@ -26,7 +53,7 @@ const checkJwt = async (req: RequestExt, res: Response, next: NextFunction) => {
         error: {
           msg: "NO_TIENES_UN_JWT_VALIDO",
         },
-      }); // Token no presente después de "Bearer"
+      });
     }
 
     const isUser = (await verifyToken(jwt)) as { id: string }; // Asegúrate de esperar la promesa
